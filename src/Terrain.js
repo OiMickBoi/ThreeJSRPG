12367172eaec231476e21a1b7b456e3d6b7d7509
oimickboi <oimickboi@gmail.com>
2024-11-17 11:53:09 -0700

    added Bushes

--- src/Terrain.js
+++ src/Terrain.js
@@ -6,13 +6,14 @@ export class Terrain extends THREE.Mesh {
 
     this.width = width;
     this.height = height;
-
     this.treeCount = 10;
     this.rockCount = 20;
+    this.bushCount = 20;
 
     this.createTerrain();
     this.createTrees();
     this.createRocks();
+    this.createBushes();
   }
 
   createTerrain() {
@@ -94,6 +95,40 @@ export class Terrain extends THREE.Mesh {
       //rockMesh.scale
       rockMesh.scale.y = height;
       this.rocks.add(rockMesh);
+    }
+  }
+
+
+  createBushes() {
+    const minBushRadius = 0.1;
+    const maxBushRadius = 0.3;
+    const minBushHeight = 0.5;
+    const maxBushHeight = 0.8;
+
+    const bushMaterial = new THREE.MeshStandardMaterial({
+      color: 0x80a040,
+      flatShading: true
+    });
+
+    this.bushs = new THREE.Group(); // a group is a collection of objects so we can group similar meshes
+    this.add(this.bushs);
+
+    this.bushs.clear();
+
+    for (let i = 0; i < this.bushCount; i++) {
+      const radius = minBushRadius + (Math.random() * (maxBushRadius - minBushRadius));
+      const height = minBushHeight + (Math.random() * (maxBushHeight - minBushHeight));
+      const bushGeometry = new THREE.SphereGeometry(radius, 8, 8);
+      const bushMesh = new THREE.Mesh(bushGeometry, bushMaterial);
+      bushMesh.position.set(
+        Math.floor(this.width * Math.random() + 0.5),
+        0,
+        Math.floor(this.height * Math.random() + 0.5)
+      );
+
+      //bushMesh.scale
+      bushMesh.scale.y = height;
+      this.bushs.add(bushMesh);
     }
   }
 
